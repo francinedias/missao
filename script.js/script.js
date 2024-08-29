c48d4c9502a6e498bab51c37920c2cab1c7f645b
francinedias <116805792+francinedias@users.noreply.github.com>
2024-08-29 10:33:51 -0300

    ok

---
+++ script.js/script.js
@@ -0,0 +1,26 @@
+const caixaprincipal= document.querySelector('caixa-principal');
+const caixaperguntas= document.querySelector('caixa-perguntas');
+const caixaalternativas= document.querySelector('caixa-alternativas');
+const caixaresultados= document.querySelector('caixa-resultados');
+
+const perguntas = [
+{
+  enunciado: "como voçe acredita que podemos combater o racismo na sociedade?"
+    alternativas:[
+        {
+            texto:"participando e campanhase concientiaçao e educacao sobre igualdade racial.",
+            afirmaçao" afirmaçao1"
+        },
+    {
+        texto: "denunciando atos de discriminaçao e apoindo politicas publica inclusivas.",
+        afirmaçao:"afirmaçao2"
+    }
+]
+{
+    enunciado:"como voçe pode ajudar a combater a disseminaçao de fake news, especialmente as que perpeuam o racimsmo?",
+    alternativas:   [
+        {
+            texto:"educando amigos e familiares sobre os perigos das fake news e incentivando os a nao compartilhar conteudos falsos que perpetuamo recismo.",
+        }
+    ]
+}
